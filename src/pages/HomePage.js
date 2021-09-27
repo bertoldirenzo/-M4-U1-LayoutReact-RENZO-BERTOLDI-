bd0b874d0a3754f590be13ee84097f20c70e56ac
bertoldirenzo <86577585+bertoldirenzo@users.noreply.github.com>
2021-09-26 22:05:02 -0300

    ajuste hojas de estilo

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,7 +8,9 @@ const HomePage = (props) => {
     return (
         <main>
             <h1>Bienvenidos</h1>
-            <img src="img/foto1.jpg" alt="Bienvenidos" />
+            <div className="imgbienvenidos">
+              <img  src="img/foto1.jpg" alt="Bienvenidos" />
+            </div>  
             <p>Bienvenido a LAB 3D el laboratorio por excelencia para realizar los diseños e impresiones 3D de la protesis
                 dentales que necesites
             </p>
